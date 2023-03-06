6dcf88ca4cd18d836b080c22f22aec367697e612
Kyle Kuo <kyle.tt.kuo@gmail.com>
2023-03-06 13:23:12 -0300

    feat: :adhesive_bandage: adds target="_blank" to links in 'Footer'

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,31 +3,39 @@
 
 		<div id="author">
 			Made by 
-			<NuxtLink class="underlined-link" to="https://kylekuo.dev/">
+			<NuxtLink 
+				to="https://kylekuo.dev/"
+				target="_blank"
+				class="underlined-link"
+			>
 				Kyle Kuo
 			</NuxtLink>
 		</div>
 		
 		<section id="credits">
 			<div id="source">
-				<NuxtLink class="underlined-link" to="https://rickandmortyapi.com/">
+				<NuxtLink 
+					to="https://rickandmortyapi.com/"
+					target="_blank"
+					class="underlined-link" 
+				>
 					<h1>Rick and Morty API</h1>
 				</NuxtLink>
 			</div>
 			<div id="stack">
-				<NuxtLink to="https://nuxt.com/">
+				<NuxtLink to="https://nuxt.com/" target="_blank">
 					<Icon name="vscode-icons:file-type-nuxt" />
 				</NuxtLink>
-				<NuxtLink to="https://vuejs.org/">
+				<NuxtLink to="https://vuejs.org/" target="_blank">
 					<Icon name="vscode-icons:file-type-vue" />
 				</NuxtLink>
-				<NuxtLink to="https://www.apollographql.com/">
+				<NuxtLink to="https://www.apollographql.com/" target="_blank">
 					<Icon name="simple-icons:apollographql" />
 				</NuxtLink>
-				<NuxtLink to="https://graphql.org/">
+				<NuxtLink to="https://graphql.org/" target="_blank">
 					<Icon name="vscode-icons:file-type-graphql" />
 				</NuxtLink>
-				<NuxtLink to="https://www.typescriptlang.org/">
+				<NuxtLink to="https://www.typescriptlang.org/" target="_blank">
 					<Icon name="vscode-icons:file-type-typescript-official" />
 				</NuxtLink>
 			</div>
