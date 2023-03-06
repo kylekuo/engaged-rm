27fe9842f2e9019272f516fe3c8ca2f9563eb2e2
Kyle Kuo <kyle.tt.kuo@gmail.com>
2023-03-06 12:28:06 -0300

    chore: :coffin: removes unused files

--- pages/episode/[id].vue
+++
@@ -1,6 +0,0 @@
-<template>
-	<main id="episode">
-		<h1>{{ $route.params.id }}</h1>
-		<h2>Episode</h2>
-	</main>
-</template>
--- pages/location/[id].vue
+++
@@ -1,6 +0,0 @@
-<template>
-	<main id="location">
-		<h1>{{ $route.params.id }}</h1>
-		<h2>Location</h2>
-	</main>
-</template>
